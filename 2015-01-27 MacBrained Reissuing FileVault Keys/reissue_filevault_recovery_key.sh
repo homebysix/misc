c2ec6f7ac771308e07428b5ba54760430cb6f080
Elliot Jordan <elliot@elliotjordan.com>
2016-09-13 12:35:38 -0700

    Standardized capitalization of variable names.

--- 2015-01-27 MacBrained Reissuing FileVault Keys/reissue_filevault_recovery_key.sh
+++ 2015-01-27 MacBrained Reissuing FileVault Keys/reissue_filevault_recovery_key.sh
@@ -62,59 +62,59 @@ LOGO_ICNS="$(/usr/bin/osascript -e 'tell application "System Events" to return P
 # https://github.com/JAMFSupport/FileVault2_Scripts/blob/master/reissueKey.sh
 
 # Check the OS version.
-OS_major=$(/usr/bin/sw_vers -productVersion | awk -F . '{print $1}')
-OS_minor=$(/usr/bin/sw_vers -productVersion | awk -F . '{print $2}')
-if [[ "$OS_major" -ne 10 || "$OS_minor" -lt 9 ]]; then
+OS_MAJOR=$(/usr/bin/sw_vers -productVersion | awk -F . '{print $1}')
+OS_MINOR=$(/usr/bin/sw_vers -productVersion | awk -F . '{print $2}')
+if [[ "$OS_MAJOR" -ne 10 || "$OS_MINOR" -lt 9 ]]; then
     echo "[ERROR] OS version not 10.9+ or OS version unrecognized."
     /usr/bin/sw_vers -productVersion
     exit 1003
 fi
 
 # Check to see if the encryption process is complete
-encryptCheck="$(/usr/bin/fdesetup status)"
-if [[ "$(echo "${encryptCheck}" | grep -c "Encryption in progress")" -gt 0 ]]; then
+FV_STATUS="$(/usr/bin/fdesetup status)"
+if [[ "$(echo "$FV_STATUS" | grep -c "Encryption in progress")" -gt 0 ]]; then
     echo "[ERROR] The encryption process is still in progress."
-    echo "${encryptCheck}"
+    echo "$FV_STATUS"
     exit 1004
-elif [[ "$(echo "${encryptCheck}" | grep -c "FileVault is Off")" -gt 0 ]]; then
+elif [[ "$(echo "$FV_STATUS" | grep -c "FileVault is Off")" -gt 0 ]]; then
     echo "[ERROR] Encryption is not active."
-    echo "${encryptCheck}"
+    echo "$FV_STATUS"
     exit 1005
-elif [[ "$(echo "${encryptCheck}" | grep -c "FileVault is On")" -eq 0 ]]; then
+elif [[ "$(echo "$FV_STATUS" | grep -c "FileVault is On")" -eq 0 ]]; then
     echo "[ERROR] Unable to determine encryption status."
-    echo "${encryptCheck}"
+    echo "$FV_STATUS"
     exit 1006
 fi
 
 # Get the logged in user's name
-userName="$(/usr/bin/stat -f%Su /dev/console)"
+CURRENT_USER="$(/usr/bin/stat -f%Su /dev/console)"
 
 # This first user check sees if the logged in account is already authorized with FileVault 2
-userCheck="$(/usr/bin/fdesetup list)"
-echo "$userCheck" | egrep -q "^${userName},"
+FV_USERS="$(/usr/bin/fdesetup list)"
+echo "$FV_USERS" | egrep -q "^$CURRENT_USER,"
 if [[ $? -ne 0 ]]; then
-    echo "[ERROR] $userName is not on the list of FileVault enabled users:"
-    echo "$userCheck"
+    echo "[ERROR] $CURRENT_USER is not on the list of FileVault enabled users:"
+    echo "$FV_USERS"
     exit 1002
 fi
 
 ################################# MAIN PROCESS #################################
 
 # Display a branded prompt explaining the password prompt.
-echo "Alerting user ${userName} about incoming password prompt..."
+echo "Alerting user $CURRENT_USER about incoming password prompt..."
 
 "$jamfHelper" -windowType "utility" -icon "$LOGO_PNG" -heading "$PROMPT_HEADING" -description "$PROMPT_MESSAGE" -button1 "Next" -defaultButton 1 -startlaunchd &>/dev/null
 
 # Get the logged in user's password via a prompt
-echo "Prompting ${userName} for their Mac password..."
-userPass="$(/usr/bin/sudo -u "$userName" /usr/bin/osascript -e 'tell application "System Events"' -e 'with timeout of 86400 seconds' -e 'display dialog "Please enter your Mac password:" default answer "" with title "'"${PROMPT_HEADING//\"/\\\"}"'" with text buttons {"OK"} default button 1 with hidden answer with icon file "'"${LOGO_ICNS//\"/\\\"}"'"' -e 'return text returned of result' -e 'end timeout' -e 'end tell')"
+echo "Prompting $CURRENT_USER for their Mac password..."
+USER_PASS="$(/usr/bin/sudo -u "$CURRENT_USER" /usr/bin/osascript -e 'tell application "System Events"' -e 'with timeout of 86400 seconds' -e 'display dialog "Please enter your Mac password:" default answer "" with title "'"${PROMPT_HEADING//\"/\\\"}"'" with text buttons {"OK"} default button 1 with hidden answer with icon file "'"${LOGO_ICNS//\"/\\\"}"'"' -e 'return text returned of result' -e 'end timeout' -e 'end tell')"
 
 # Thanks to James Barclay for this password validation loop.
 TRY=1
-until dscl /Search -authonly "$userName" "$userPass" &> /dev/null; do
+until dscl /Search -authonly "$CURRENT_USER" "$USER_PASS" &> /dev/null; do
     (( TRY++ ))
-    echo "Prompting ${userName} for their Mac password (attempt $TRY)..."
-    userPass="$(/usr/bin/sudo -u "$userName" /usr/bin/osascript -e 'tell application "System Events"' -e 'with timeout of 86400 seconds' -e 'display dialog "Sorry, that password was incorrect. Please try again:" default answer "" with title "'"${PROMPT_HEADING//\"/\\\"}"'" with text buttons {"OK"} default button 1 with hidden answer with icon file "'"${LOGO_ICNS//\"/\\\"}"'"' -e 'return text returned of result' -e 'end timeout' -e 'end tell')"
+    echo "Prompting $CURRENT_USER for their Mac password (attempt $TRY)..."
+    USER_PASS="$(/usr/bin/sudo -u "$CURRENT_USER" /usr/bin/osascript -e 'tell application "System Events"' -e 'with timeout of 86400 seconds' -e 'display dialog "Sorry, that password was incorrect. Please try again:" default answer "" with title "'"${PROMPT_HEADING//\"/\\\"}"'" with text buttons {"OK"} default button 1 with hidden answer with icon file "'"${LOGO_ICNS//\"/\\\"}"'"' -e 'return text returned of result' -e 'end timeout' -e 'end tell')"
     if [[ $TRY -ge 5 ]]; then
         echo "[ERROR] Password prompt unsuccessful after 5 attempts."
         exit 1007
@@ -128,27 +128,27 @@ launchctl unload /System/Library/LaunchDaemons/com.apple.security.FDERecoveryAge
 echo "Issuing new recovery key..."
 # Translate XML reserved characters to XML friendly representations.
 # Thanks @AggroBoy! - https://gist.github.com/AggroBoy/1242257
-userPassXMLFriendly=$(echo "$userPass" | sed -e 's~&~\&amp;~g' -e 's~<~\&lt;~g' -e 's~>~\&gt;~g' -e 's~\"~\&quot;~g' -e "s~\'~\&apos;~g" )
+USER_PASS_XML=$(echo "$USER_PASS" | sed -e 's~&~\&amp;~g' -e 's~<~\&lt;~g' -e 's~>~\&gt;~g' -e 's~\"~\&quot;~g' -e "s~\'~\&apos;~g" )
 fdesetup changerecovery -norecoverykey -verbose -personal -inputplist << EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
     <key>Username</key>
-    <string>$userName</string>
+    <string>$CURRENT_USER</string>
     <key>Password</key>
-    <string>$userPassXMLFriendly</string>
+    <string>$USER_PASS_XML</string>
 </dict>
 </plist>
 EOF
 
-result=$?
-if [[ $result -ne 0 ]]; then
-    echo "[WARNING] fdesetup exited with return code: $result."
+RESULT=$?
+if [[ $RESULT -ne 0 ]]; then
+    echo "[WARNING] fdesetup exited with return code: $RESULT."
 fi
 
 echo "Loading FDERecoveryAgent..."
 # `fdesetup changerecovery` should do this automatically, but just in case...
 launchctl load /System/Library/LaunchDaemons/com.apple.security.FDERecoveryAgent.plist &>/dev/null
 
-exit $result
+exit $RESULT
